e46afc2b3f7d902b18a71a277a1e5b04cb347533
Waldemar Adams <waldek-adamski@wp.pl>
2020-06-04 17:04:48 +0200

    assign variables to elements, set function on "btn"

---
+++ script/main.js
@@ -0,0 +1,14 @@
+const lettersToChoice = document.querySelectorAll('div.letters div');
+const drawWord = document.querySelector('div.draw__word');
+const btn = document.querySelector('form button');
+const colors = ['blue', 'red', 'white', 'orange'];
+const animals = ['cat', 'hamster', 'pig', 'sheep'];
+const fruits = ['apple', 'cherry', 'pear', 'banana'];
+const drawnWordArray = [];
+
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+})
