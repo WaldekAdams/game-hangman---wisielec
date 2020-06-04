b952c1759f959cf6a8aacd2790b6b8e1c7647755
Waldemar Adams <waldek-adamski@wp.pl>
2020-06-04 17:21:31 +0200

    btn function:draw a word from array

--- script/main.js
+++ script/main.js
@@ -9,6 +9,26 @@ const drawnWordArray = [];
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
+    let selectedArray;
+    const input = document.querySelector('input[type="radio"]:checked');
+
+    // specify witch option(array) the player has chosen
+
+    if (input.value == 'colors') {
+        selectedArray = colors;
+    } else if (input.value == 'animals') {
+        selectedArray = animals;
+    } else if (input.value == 'fruit') {
+        selectedArray = fruits;
+    }
+    // console.log(selectedArray)
+
+    //draw a words from the option(array)
+
+    let index = Math.floor(Math.random() * selectedArray.length);
+    let color = selectedArray[index];
+    // console.log(color);
+
 
 
 })
